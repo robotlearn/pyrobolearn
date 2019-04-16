f3a49a9f217eff9e1da1e4a8fffeb59ba9df9abe
Brian Delhaisse <briandelhaisse@gmail.com>
2019-04-16 15:08:42 +0200

    correct/update setup.py

--- setup.py
+++ setup.py
@@ -24,18 +24,25 @@ setup(
     version='0.1.0',
     description='A Python framework for roboticists and machine learning practitioners',
     long_description = long_description,
+    long_description_content_type="text/markdown",
     author='Brian Delhaisse',
     author_email='briandelhaisse@gmail.com',
     maintainer='Brian Delhaisse',
-    maintainer_email='Brian Delhaisse',
+    maintainer_email='briandelhaisse@gmail.com',
     license='(c) Brian Delhaisse',
     url='https://github.com/robotlearn/pyrobolearn',
     platforms=['Linux Ubuntu'],
-    # python_requires='==2.7.*',
+    # python_requires='==2.7.*',
     # packages=['', 'worlds', 'robots', 'tools', 'pso', 'envs', 'algos', 'optim', 'tasks', 'tools.vr', 'tools.vr.htc', 'tools.vr.oculus',
     #           'tools.bci', 'tools.audio', 'tools.camera', 'tools.game_controllers', 'utils', 'utils.data_structures',
     #           'models', 'robots', 'robots.ros', 'robots.ros.coman', 'robots.ros.walkman', 'filters',
     #           'metrics', 'rl', 'mechanics', 'objectives', 'experiments', 'exploration'],
     packages=find_packages(),  #find_packages(exclude=('tests',))
-    install_requires=reqs
+    install_requires=reqs,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
