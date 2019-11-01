6447c88a2c7bbc43ec60beff1786800c994ac222
Brian Delhaisse <briandelhaisse@gmail.com>
2019-11-01 22:10:05 +0100

    add file for RMP (todo)

---
+++ pyrobolearn/models/rmp/rmp.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+r"""Define the Riemannian Motion Policies
+
+"The Riemannian Motion Policy (RMP) is an acceleration field (dynamical system) coupled with a corresponding Riemannian metric defining directions of importance at each point, typically defined on a nonlinear task space." [1]
+
+References:
+    - [1] "Riemannian Motion Policies", Ratliff et al., 2018
+    - [2] "RMPflow: A Computational Graph for Automatic Motion Policy Generation", Cheng et al., 2018
+
+Implementations found online:
+    - RMP in ROS: https://github.com/rortiz9/rmp-ros
+    - Multi-Robot RMPflow: https://github.com/gtrll/multi-robot-rmpflow
+"""
+
+# TODO: read and implement the RMP
+
+from pyrobolearn.models.model import Model
+
+
+class RMP(object):  # Model
+    r"""Riemannian Motion Policies
+
+    References:
+        - [1] "Riemannian Motion Policies", Ratliff et al., 2018
+        - [2] "RMPflow: A Computational Graph for Automatic Motion Policy Generation", Cheng et al., 2018
+    """
+
+    def __init__(self):
+        pass
+
